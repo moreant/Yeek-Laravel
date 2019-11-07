16f182314e680ab84332c5ff6174b814be7134b5
mojuchen <552191481@qq.com>
2019-11-07 10:44:12 +0800

    修复备案地址错误

--- resources/views/layouts/work.blade.php
+++ resources/views/layouts/work.blade.php
@@ -101,8 +101,8 @@
     <footer class="jumbotron" style="margin:0;padding:2rem 1rem; max-height:300px;">
         <div class="container">
             <a href="http://www.yeek.top">Yeek.top</a>@2019<br>
-            ICP主体备案号：<a class="text-secondary" href="http://www.yeek.top">粤ICP备19003211号</a><br>
-            粤公网安备：<a class="text-secondary" href="http://www.yeek.top">44011102002523</a><br>
+            ICP主体备案号：<a class="text-secondary" href="http://gd.beian.miit.gov.cn/publish/query/indexFirst.action">粤ICP备19003211号</a><br>
+            粤公网安备：<a class="text-secondary" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011102002523">44011102002523</a><br>
         </div>
     </footer>
     @show
