6f254005445fce4349f4b1f8763a3644ff94527c
mojuchen <552191481@qq.com>
2019-11-11 17:48:30 +0800

    页脚大改

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,7 +1,42 @@
+<style>
+    footer a {
+        color: #6c757d !important;
+    }
+</style>
 <footer class="jumbotron" style="margin:0;padding:2rem 1rem; max-height:300px;">
     <div class="container">
-        <a href="http://www.yeek.top">Yeek.top</a>@2019<br>
-        ICP主体备案号：<a class="text-secondary" target="_blank" href="http://beian.miit.gov.cn/">粤ICP备19003211号</a><br>
-        粤公网安备：<a class="text-secondary" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011102002523">44011102002523</a><br>
+        <div class="row mb-3">
+            <div class="col-sm-4">
+                <h4>站长</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">莫居尘 - 学生</a></li>
+                    <li><a href="#">552191481@qq.com</a></li>
+                    <li><a href="https://mojuchen.github.io/">个人博客</a></li>
+
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <h4>关于</h4>
+                <ul class="list-unstyled">
+                    <li><a href="https://github.com/mojuchen/Yeek-Laravel/commits/master">更新日志</a></li>
+                    <li><a href="https://github.com/mojuchen/Yeek-Laravel">项目开源</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <h4>友情链接</h4>
+                <ul class="list-unstyled">
+                    <li><a href="http://ip.yeek.top/">旧版Yeek</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="text-center small">
+            学习即输入输出
+            <br>
+            <a target="_blank" href="http://beian.miit.gov.cn/">粤ICP备19003211号-1</a>
+            <span class="ml-2 mr-2">|</span>
+            <a target="_blank"
+                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011102002523">粤公网安备44011102002523</a>
+        </div>
+
     </div>
 </footer>
