02397b181347a791fb62c60a7879cc5a3946628e
mojuchen <552191481@qq.com>
2019-11-23 21:53:33 +0800

    :lipstick: 首页添加架构介绍

--- resources/views/myWelcome.blade.php
+++ resources/views/myWelcome.blade.php
@@ -25,11 +25,21 @@
             background-size: cover;
         }
 
-        h4>a{
+        #home-nav a {
             color: #343a40;
         }
-        h4>a:hover {
-            color: #0257b1;
+
+        #home-nav div a:hover {
+            color: inherit;
+            text-decoration: none
+        }
+
+        #home-nav div a:hover {
+            background: #e3e3e3;
+        }
+
+        .media-img {
+            max-width: 240px;
         }
     </style>
 </head>
@@ -39,6 +49,7 @@
 
     @endcomponent
     <main class="flex-md-grow-1">
+        <!-- 轮播图 -->
         <div id="banner" class="carousel slide" data-ride="carousel">
             <!-- 指示符 -->
             <ul class="carousel-indicators">
@@ -90,15 +101,82 @@
 
         </div>
 
-        <div class="row text-center w-100 m-0">
-            <div class="col-md bg-light border p-4">
-                <h4 class="font-weight-bold"><a href="/work">作业管理</a></h4>
+        <!-- 首页导航栏 -->
+        <div id="home-nav" class="text-center bg-light ">
+            <div class="container row mx-auto">
+                <a class="col-md p-4" href="/work">
+                    <h4 class="font-weight-bold">作业管理</h4>
+                </a>
+                <a class="col-md p-4" href="/ftp">
+                    <h4 class="font-weight-bold">FTP列表</h4>
+                </a>
+                <a class="col-md p-4" href="/ftp">
+                    <h4 class="font-weight-bold">个人中心</h4>
+                </a>
             </div>
-            <div class="col-md bg-light border p-4">
-                <h4 class="font-weight-bold"><a href="/ftp">FTP列表</h4>
+        </div>
+
+        <!-- 媒体栏 -->
+        <div id="media-box" class="">
+            <br>
+            <h1 class="text-center">本站架构</h1>
+            <div class="container p-5">
+                <hr>
+                <div class="d-flex flex-lg-row flex-column p-5 justify-content-around align-items-center">
+                    <div><img src="img/aliyun.png" class="media-img"></div>
+                    <div>
+                        <h2 class="mb-3 mt-3">云服务器</h2>
+                        <h4>使用阿里云 ECS 云服务</h4>
+                        <h4>7*24 小时运行，高性能云盘</h4>
+                    </div>
+                </div>
             </div>
-            <div class="col-md bg-light border p-4">
-                <h4 class="font-weight-bold"><a href="/ftp">个人中心</h4>
+            <div class="bg-light">
+                <div class="container p-5">
+                    <div class="d-flex flex-lg-row flex-column-reverse p-5 justify-content-around align-items-center ">
+                        <div>
+                            <h2 class="mb-3 mt-3">融合 CDN</h2>
+                            <h4>七牛云融合 CDN 加速</h4>
+                            <h4>近获取所需内容，更快、更稳定</h4>
+                        </div>
+                        <div><img src="img/qiniu.png" class="media-img" alt=""></div>
+                    </div>
+                </div>
+            </div>
+            <div class="container p-5">
+                <div class="d-flex flex-lg-row flex-column p-5 justify-content-around align-items-center ">
+                    <div><img src="img/docker.png" class="media-img" alt=""></div>
+                    <div>
+                        <h2 class="mb-3 mt-3">Docker</h2>
+                        <h4>使用 Docker 部署 LNMP 环境</h4>
+                        <h4>标准化快速部署，资源开销低</h4>
+                    </div>
+
+                </div>
+            </div>
+            <div class="bg-light">
+                <div class="container p-5">
+                    <div class="d-flex flex-lg-row flex-column-reverse p-5 justify-content-around align-items-center">
+                        <div>
+                            <h2 class="mb-3 mt-3">Nginx</h2>
+                            <h4>选择高性能的 HTTP WEB 服务器</h4>
+                            <h4>配合云解析 DNS，显著降低应答延迟</h4>
+                        </div>
+                        <div><img src="img/nginx.png" class="media-img" alt=""></div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="container p-5">
+                <div class="d-flex flex-lg-row flex-column p-5 justify-content-around align-items-center ">
+                    <div><img src="img/laravel.png" class="media-img" alt="" style=""></div>
+                    <div>
+                        <h2 class="mb-3 mt-3">Laravel</h2>
+                        <h4>优雅的 PHP Web 开发框架</h4>
+                        <h4>采用 MVC 设计，是最受关注的 PHP 框架</h4>
+                    </div>
+
+                </div>
             </div>
         </div>
 
