8b4f59a969a2e76785d7150b11b89d53ff50a7f3
moreant <552191481@qq.com>
2020-01-27 15:09:56 +0800

    备案

--- resources/views/myWelcome.blade.php
+++ resources/views/myWelcome.blade.php
@@ -44,7 +44,7 @@
                 <a href="https://getbootstrap.com/docs/4.4/examples/cover/">mdo</a>
                
                 <span class="mx-2">|</span>
-                粤ICP备19003211号-1
+                <a href="http://http://beian.miit.gov.cn/">粤ICP备19003211号</a>
                 <span class="mx-2">|</span>
                 <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011102002523">
                     粤公网安备44011102002523号
